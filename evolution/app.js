9b52bba1f1449422594bebb07161b595a326632f
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-11-29 14:57:35 +0530

    Update app.js

--- evolution/app.js
+++ evolution/app.js
@@ -6,6 +6,28 @@ const connect = ()=>{
     return mongoose.connect("mongodb+srv://ab360:mongo2244@cluster0.ntjha.mongodb.net/test")
 }
 
+// schema for jobs
+
+const jobSchema = new mongoose.Schema({
+    job_title:{type:String, required:true, unique:true},
+    rating:{type:Number, required:true},
+
+
+},{
+    versionKey:false,
+    timeStamps: true
+})
+
+const Job = mongoose.model("job",jobSchema);
+
+//schema for city
+
+const citySchema = new mongoose.Schema({
+    city_name:{type:String, required:true, unique:true}
+})
+
+
+
 const app = express();
 app.use(express.json());
 
