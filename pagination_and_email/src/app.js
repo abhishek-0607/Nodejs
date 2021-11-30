b2bd7b6957184de583b58b9b382cab5ce4ece5fc
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-01 01:18:17 +0530

    Update app.js

--- pagination_and_email/src/app.js
+++ pagination_and_email/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const usersController = require("../controllers/user_controller");
+const usersController = require("./controllers/user_controller");
 
 const app = express();
 app.use(express.json());
