68533bb15c1d702e4f4dc40b68a1fbce450994b7
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-01 00:59:20 +0530

    Create app.js

---
+++ pagination_and_email/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const usersController = require("../controllers/user_controller");
+
+const app = express();
+app.use(express.json());
+
+app.use("/users",usersController)
+
+module.exports = app;
