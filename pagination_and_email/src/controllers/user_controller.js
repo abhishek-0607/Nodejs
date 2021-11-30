2503312a375be248f5e1975e2c4d1cd2366e1564
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-01 01:53:43 +0530

    Update user_controller.js

--- pagination_and_email/src/controllers/user_controller.js
+++ pagination_and_email/src/controllers/user_controller.js
@@ -15,8 +15,12 @@ router.post("/",async(req,res)=>{
 })
  router.get("/",async(req,res)=>{
      try{
-        const users = await User.find().lean().exec()
-        return res.send(users);
+        const page = +req.query.page || 1;
+        const size = +req.query.size || 2;
+        const skip = (page -1)*size;
+        const users = await User.find().skip(skip).limit(size).lean().exec();
+        const totalPages = Math.ceil((await User.find().countDocuments())/size)
+        return res.json({users, totalPages});
      }
      catch(e){
          return res.status(500).json({status:"Failed", message:e.message})
