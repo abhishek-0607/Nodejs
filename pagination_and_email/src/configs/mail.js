6a7ac50c8655914f6a9ddc110734d66adc8a8e99
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-02 02:30:29 +0530

    Create mail.js

---
+++ pagination_and_email/src/configs/mail.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+});
+module.exports = transporter;
