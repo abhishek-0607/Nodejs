4f9753247b8ea11050602ee15c6d0fc5e5454a8d
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-04 00:04:16 +0530

    Update index.js

--- file_uploads/src/index.js
+++ file_uploads/src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const userController = require("./controllers/user_controller")
+const galleryController = require("./controllers/gallery_controller")
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use("/users",userController)
+app.use("/galleries",galleryController);
 
 module.exports = app;
