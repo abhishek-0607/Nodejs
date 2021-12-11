d23667b65e952b803a4654427b3794f924b1210a
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-11 21:57:35 +0530

    Create product_controller.js

---
+++ googleauth/src/controllers/product_controller.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const authenticate = require("../middlewares/authenticate");
+
+const Product = require("../models/product");
+
+const router = express.Router();
+
+
+router.post("/",authenticate,async (req,res)=>{
+    try{
+        const user = req.user;
+        console.log(user)
+
+
+        const product = await Product.create({
+            name: req.body.name,
+            price: req.body.price,
+            user: user.user._id
+        });
+
+       return res.status(201).send(product);
+    }
+    catch(e){
+       return res.status(500).json({ message:e.message, status:"failed" })
+    }
+
+})
+
+
+router.get("/",async (req,res)=>{
+    try{
+        const products = await Product.find().lean().exec()
+       return res.send({products});
+    }
+    catch(e){
+       return res.status(500).json({ message: e.message, status:"failed" })
+    }
+
+})
+
+router.get("/:id", async (req,res)=>{
+    try{
+        const product = await Product.findById(req.params.id).lean().exec();
+       return res.send(product);
+    }catch(e){
+       return res.status(500).json({ message: e.message, status:"failed" })
+
+    }
+});
+
+module.exports = router;
