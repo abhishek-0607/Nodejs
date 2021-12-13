ed085d234bd62096d17bd08c8f6871a839ad186b
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 16:08:39 +0530

    Create authenticate.js

---
+++ evolution_13Dec/src/middlewares/authenticate.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (token) =>{
+    return new Promise((resolve,reject)=>{
+        jwt.verify(token,process.env.JWT_ACCESS_KEY,function(err,token){
+            if(err) return reject(err)
+            return resolve(token);
+        })
+    })
+}
+
+module.exports = async (req,res,next)=>{
+    const bearerToken = req?.headers?.authorization;
+
+    if(!bearerToken || bearerToken.startWith("Bearer "))
+    return res.status(400).json({
+        status:"Failed",
+        message: "Please provide a valid token"
+    })
+
+    const token = bearertoken.split(" ")[1];
+
+    let user 
+    try{
+        user = await verifyToken(token);
+
+    }catch(e){
+        return res.status(400).json({
+            status:"Failed",
+            message: "Please provide a valid token"
+        })
+    }
+
+    if(!user)
+    return res.status(400).json({
+        status:"Failed",
+            message: "Please provide a valid token"
+    })
+
+    req.user = user
+
+    return next();
+
+}
