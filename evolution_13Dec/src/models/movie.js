693b2338a6312383bc2f8ba1eacd1cddec7abc3b
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 14:46:04 +0530

    Create movie.js

---
+++ evolution_13Dec/src/models/movie.js
@@ -0,0 +1,17 @@
+const {Schema, model} = require("mongoose");
+
+const movieSchema = new Schema (
+    {
+        name:{type:String,required:true},
+        actors:[{type:String,required:true}], 
+        directors:[{type:String,required:true}],
+        languages:[{type:String,required:true}],
+        poster_url:[{type:String,required:true}]
+    },
+    {
+        versionKey:false,
+        timestamps:true
+    })
+
+const Movie = model("movie", movieSchema);
+module.exports = Movie;
