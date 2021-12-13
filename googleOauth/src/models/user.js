05e0f21acadc0d3d2dc010d8f936f45b97b93202
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 10:32:25 +0530

    Update user.js

--- googleOauth/src/models/user.js
+++ googleOauth/src/models/user.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcryptjs")
 const userSchema = new Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
-
+    password:{type:String,required:true},
+    roles:[{type:String, required:true}]
 },{
     versionKey:false,
     timestamps:true
