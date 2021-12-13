bedd88006d73664ad4aa02bc07607dbe1ee813db
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 16:44:53 +0530

    Update index.js

--- evolution_13Dec/src/index.js
+++ evolution_13Dec/src/index.js
@@ -9,6 +9,8 @@ const movieController = require("./controllers/movie_controller");
 const theatreController = require("./controllers/theatre_controller");
 
 const screenController = require("./controllers/screen_controller");
+const showController = require("./controllers/show_controller");
+
 
 
 
@@ -24,6 +26,8 @@ app.post("/login", login);
 app.use("/movies",movieController)
 app.use("/theatres",theatreController)
 app.use("/screens",screenController)
+app.use("/shows",showController)
+
 
 
 
