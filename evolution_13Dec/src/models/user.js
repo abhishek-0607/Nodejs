f2466e6583a765b78dead1398806f55c9a34178d
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 16:30:41 +0530

    Update user.js

--- evolution_13Dec/src/models/user.js
+++ evolution_13Dec/src/models/user.js
@@ -7,7 +7,8 @@ const userSchema = new Schema (
         name:{type:String,required:true},
         email:{type:String,required:true,unique:true},
         password:{type:String,required:true},
-
+        profile_photo_url:{type:String},
+        roles:[{type:String}]
     },
     {
         versionKey:false,
