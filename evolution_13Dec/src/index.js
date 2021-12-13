9c2286dce74b9eee9fed7ed0d243047ee895a93f
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 15:08:12 +0530

    Update index.js

--- evolution_13Dec/src/index.js
+++ evolution_13Dec/src/index.js
@@ -4,6 +4,11 @@ const userController = require("./controllers/user_controller");
 
 const movieController = require("./controllers/movie_controller");
 
+const theatreController = require("./controllers/theatre_controller");
+
+const screenController = require("./controllers/screen_controller");
+
+
 
 
 
@@ -12,6 +17,9 @@ app.use(express.json());
 
 app.use("/users",userController)
 app.use("/movies",movieController)
+app.use("/theatres",theatreController)
+app.use("/screens",screenController)
+
 
 
 module.exports = app;
