208c3449713bedb6183c9ea764897a4b6be52de4
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 16:30:12 +0530

    Update movie_controller.js

--- evolution_13Dec/src/controllers/movie_controller.js
+++ evolution_13Dec/src/controllers/movie_controller.js
@@ -2,14 +2,18 @@ const express = require("express");
 
 const Movie = require("../models/movie");
 
+//const authenticate = require("../middlewares/authenticate")
+
 const router = express.Router();
 
 router.post("/",async (req,res)=>{
     try{
+
+        //const user = req.user
         const movie = await Movie.create(req.body);
-        return res.status(201).send(movie)
+        return res.status(201).send({movie})
     }
-    catch{
+    catch(e){
         return res.status(500).json({message:e.message, status :"Failed"})
     }
 })
