6596d9efa96be9432fdbee32a7a2576551a05c0b
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 14:46:02 +0530

    Update index.js

--- evolution_13Dec/src/index.js
+++ evolution_13Dec/src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
+const userController = require("./controllers/user_controller");
+
+const movieController = require("./controllers/movie_controller");
+
+
+
+
 const app = express();
 app.use(express.json());
+
+app.use("/users",userController)
+app.use("/movies",movieController)
+
 
 module.exports = app;
