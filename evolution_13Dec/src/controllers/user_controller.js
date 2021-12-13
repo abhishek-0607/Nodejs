c6b2693b97293554b42aff61fa86fde4d9b01ce0
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 14:46:00 +0530

    Update user_controller.js

---
+++ evolution_13Dec/src/controllers/user_controller.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.post("/",async (req,res)=>{
+    try{
+        const user = await User.create(req.body);
+        return res.status(201).send(user)
+    }
+    catch{
+        return res.status(500).json({message:e.message, status :"Failed"})
+    }
+})
+
+module.exports = router;
