7ca0655dba4bef154fb98d645bad65586ac59d43
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2021-12-13 16:30:19 +0530

    Update movie.js

--- evolution_13Dec/src/models/movie.js
+++ evolution_13Dec/src/models/movie.js
@@ -3,10 +3,10 @@ const {Schema, model} = require("mongoose");
 const movieSchema = new Schema (
     {
         name:{type:String,required:true},
-        actors:[{type:String,required:true}], 
-        directors:[{type:String,required:true}],
-        languages:[{type:String,required:true}],
-        poster_url:[{type:String,required:true}]
+        actors:[{type:String}], 
+        directors:[{type:String}],
+        languages:[{type:String}],
+        poster_url:[{type:String}],
     },
     {
         versionKey:false,
