25be054abf0c14ca1b05f8c7ea4c70d5c297a3a9
Abhishek Silawat <absilawat@gmail.com>
2022-09-03 13:57:26 +0530

    Update user.js

--- authentication/src/models/user.js
+++ authentication/src/models/user.js
@@ -1,39 +1,39 @@
-const {Schema,model}= require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const userSchema = new Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
 
-},{
-    versionKey:false,
-    timestamps:true
-})
-userSchema.pre("save", function (next){
+  //const hash = bcrypt.hashSync(this.password,10);
+  bcrypt.hash(this.password, 10, (res, hash) => {
+    this.password = hash;
+    return next();
+  });
+});
 
-    if(!this.isModified("password")) return next();
-
-    //const hash = bcrypt.hashSync(this.password,10);
-    bcrypt.hash(this.password,10,(res,hash)=>{
-        this.password = hash;
-        return next();
-    })
-    
-})
-
-userSchema.methods.checkPassword = function (password){
-    return new Promise((resolve,reject)=>{
-        bcrypt.compare(password, this.password, function(err, same) {
-            // res === true
-            if(err){
-                return reject(err);
-            }
-            resolve(same);
-        })
+userSchema.methods.checkPassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, function (err, same) {
+      // res === true
+      if (err) {
+        return reject(err);
+      }
+      resolve(same);
     });
-}
+  });
+};
 
-const User = model("user",userSchema);
+const User = model("user", userSchema);
 module.exports = User;
