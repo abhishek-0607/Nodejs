03f651fd77f622f43f66286b1e6782a8a68e0317
Abhishek Silawat <absilawat@gmail.com>
2022-09-03 13:57:30 +0530

    Update server.js

--- authentication/src/server.js
+++ authentication/src/server.js
@@ -2,7 +2,7 @@ const app = require("./index");
 
 const connect = require("./configs/db");
 
-app.listen(8888,async function(){
-    await connect();
-    console.log("Listening to port 8888")
-})
+app.listen(8000, async function () {
+  await connect();
+  console.log("Listening to port 8000");
+});
