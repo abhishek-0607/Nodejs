f75ff8686f76dc3d268d7da70f66f60f261f0ce5
Abhishek Silawat <absilawat@gmail.com>
2023-04-27 12:13:31 +0530

    UPDATE: connection stablished with user

--- websockets/index.js
+++ websockets/index.js
@@ -1,13 +1,32 @@
 const http = require("http");
 const { Server } = require("socket.io");
+const fs = require("fs");
+const { log } = require("console");
 
-const httpserver = http.createServer((req, res) => {
-  res.end("hello");
+const httpServer = http.createServer((req, res) => {
+  const index = fs.readFileSync(__dirname + "/index.html", "utf-8");
+  res
+    // .writeHead(200, {
+    //   "Content-type": "text/html",
+    // })
+    .end(index);
 });
-const ws = new Server(httpserver);
-ws.on("connection", (ws) => {
-  console.log("a new user connected");
+let totalUsers = 0;
+const ws = new Server(httpServer);
+ws.on("connection", (conn) => {
+  totalUsers += 1;
+  // conn.on("disconnect", () => {
+  //   totalUsers -= 1;
+  //   console.log("a user disconnected! Total Users->", totalUsers);
+  // });
+
+  conn.on("new message", (message) => {
+    //emitting the event on the server which is emitted by a User
+    ws.emit("new message", message);
+    // console.log("client message->", message);
+  });
+  console.log("a new user connected! Total users->", totalUsers);
 });
-httpserver.listen(8000, () => {
-  console.log("server started");
+httpServer.listen(8000, () => {
+  console.log("Server Started");
 });
